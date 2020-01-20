83dbca841a32a8348247276279cb6e706b2661bd
Bogosel Daniel <daniel.bogosel@fortech.ro>
2020-01-20 17:53:33 +0200

    initial setup

---
+++ vacante-backend/vacanta/src/main/java/com/liceenta/vacanta/VacantaApplication.java
@@ -0,0 +1,13 @@
+package com.liceenta.vacanta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacantaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacantaApplication.class, args);
+	}
+
+}
---
+++ vacante-backend/vacanta/src/test/java/com/liceenta/vacanta/VacantaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liceenta.vacanta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
